0908096c91ccad227952fbf1fd1334f5cb69ea08
AlexVplle <alexisvapaille11@gmail.com>
2026-05-20 01:02:58 +0200

    Add context and page table address to process control block

--- ferrum/src/process/process_control_block.rs
+++ ferrum/src/process/process_control_block.rs
@@ -1,6 +1,9 @@
 use super::state::ProcessState;
+use crate::arch::Context;
 
 struct ProcessControlBlock {
     identifier: u64,
     state: ProcessState,
+    context: Context,
+    page_table_address: u64,
 }
